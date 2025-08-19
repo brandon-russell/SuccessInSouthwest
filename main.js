a64f706ddfd9fd9248037e71a7e32d108c1bfe47
Brandon Russell <52283255+brandon-russell@users.noreply.github.com>
2025-08-19 11:05:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,54 +14,139 @@
                             $(this).parent().addClass("current-menu-item").siblings().removeClass("current-menu-item");
                         }
                     });
-                    $(".poll-btn").click(function() {
-                        $(".poll-wrapper").toggleClass("vote");
-                    });
-                    var bodyOvrelay = $("#body-overlay");
-                    var searchPopup = $("#search-popup");
-                    $(document).on("click", "#body-overlay, .border-none", function(e) {
-                        e.preventDefault();
-                        bodyOvrelay.removeClass("active");
-                        searchPopup.removeClass("active");
-                    });
-                    $(document).on("click", "#search", function(e) {
-                        e.preventDefault();
-                        searchPopup.addClass("active");
-                        bodyOvrelay.addClass("active");
-                    });
-                    new WOW().init();
-                    $(".video-play,.video-popup,.small-vide-play-btn").magnificPopup({
-                        type: "video"
-                    });
-                    $(".image-popup").magnificPopup({
-                        type: "image"
-                    });
-                    $(document).on("click", ".back-to-top", function() {
-                        $("html,body").animate({
-                            scrollTop: 0
-                        }, 2000);
-                    });
-                    $(".count-num").counterUp({
-                        delay: 20,
-                        time: 1000
-                    });
-                    if ($(".outer").length > 0) {
-                        VanillaTilt.init(document.querySelectorAll(".outer"), {
-                            max: 80,
-                            speed: 400,
-                            perspective: 200,
-                            scale: 1.2,
-                            reverse: true
-                        });
-                    }
-                    if ($("#mycountdown").length > 0) {
-                        $("#mycountdown").countdown("2024/09/20", function(event) {
-                                    $(".month").text(event.strftime("%m"));
-                                    $(".days").text(event.strftime("%n"));
-                                    $(".hours").text(event.strftime("%H"));
-                                    $(".mins").text(event.strftime("%M"));
-                                    $(".secs").text(event.strftime("%s"));
-                        });
-                    }
+        // Poll Btn
+        $(".poll-btn").click(function() {
+            $(".poll-wrapper").toggleClass("vote");
         });
- });
+        /*----------------------
+            Search Popup
+        -----------------------*/
+        var bodyOvrelay = $('#body-overlay');
+        var searchPopup = $('#search-popup');
+
+        $(document).on('click', '#body-overlay', function(e) {
+            e.preventDefault();
+            bodyOvrelay.removeClass('active');
+            searchPopup.removeClass('active');
+        });
+        $(document).on('click', '.border-none', function(e) {
+            e.preventDefault();
+            bodyOvrelay.removeClass('active');
+            searchPopup.removeClass('active');
+        });
+        $(document).on('click', '#search', function(e) {
+            e.preventDefault();
+            searchPopup.addClass('active');
+            bodyOvrelay.addClass('active');
+        });
+
+
+        /*--------------------
+            wow js init
+        ---------------------*/
+        new WOW().init();
+
+        /*-------------------------
+            magnific popup activation
+        -------------------------*/
+        $('.video-play,.video-popup,.small-vide-play-btn').magnificPopup({
+            type: 'video'
+        });
+        $('.image-popup').magnificPopup({
+            type: 'image'
+        });
+
+        /*------------------
+            back to top
+        ------------------*/
+        $(document).on('click', '.back-to-top', function() {
+            $("html,body").animate({
+                scrollTop: 0
+            }, 2000);
+        });
+
+        /*------------------------------
+            counter section activation
+        -------------------------------*/
+        var counternumber = $('.count-num');
+        counternumber.counterUp({
+            delay: 20,
+            time: 1000
+        });
+        // MouseEvent
+        var $mosueOverEffect = $('.outer');
+        if ($mosueOverEffect.length > 0) {
+            VanillaTilt.init(document.querySelectorAll(".outer"), {
+                max: 80,
+                speed: 400,
+                perspective: 200,
+                scale: 1.2,
+                reverse: true,
+            });
+        }
+        /**-----------------------------
+         *  countdown
+         * ---------------------------*/
+        if ($("#mycountdown").length > 0) {
+            $("#mycountdown").countdown("2024/09/20", function(event) {
+                $('.month').text(
+                    event.strftime('%m')
+                );
+                $('.days').text(
+                    event.strftime('%n')
+                );
+                $('.hours').text(
+                    event.strftime('%H')
+                );
+                $('.mins').text(
+                    event.strftime('%M')
+                );
+                $('.secs').text(
+                    event.strftime('%S')
+                );
+            });
+
+        }
+        // Clinet - active
+        $('.client-active-area').owlCarousel({
+                loop: true,
+                items: 4,
+                nav: true,
+                margin: 100,
+                dots: false,
+                navText: ['<span data-icon="&#x23;"></span>', '<span data-icon="&#x24;"></span>'],
+                responsive: {
+                    0: {
+                        items: 2
+                    },
+                    600: {
+                        items: 3
+                    },
+                    992: {
+                        items: 4
+                    },
+                    1200: {
+                        items: 4
+                    }
+                }
+            });
+
+    // Menu Submenu Dropdown style
+    $(".menu-item-has-children a").on("click", function() {
+        var element = $(this).parent("li");
+        if (element.hasClass("show")) {
+            element.removeClass("show");
+            element.children("ul").slideUp(500);
+        } else {
+            element.siblings("li").removeClass('show');
+            element.addClass("show");
+            element.siblings("li").find("ul").slideUp(500);
+            element.children('ul').slideDown(500);
+        }
+    });
+
+    $(window).on('scroll', function() {
+        submenuReverse();
+    });
+        
+})(jQuery);

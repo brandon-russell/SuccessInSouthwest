9ff001af900bcbaa3f61dd2f670ba31aafe6c662
Brandon Russell <52283255+brandon-russell@users.noreply.github.com>
2025-08-21 22:43:18 -0400

    Delete preloader.js

--- preloader.js
+++
@@ -1,5 +0,0 @@
- window.addEventListener("load", () => {
-    const preloader = document.getElementById("preloader");
-    preloader.classList.add("fade-out");
-    document.body.style.overflow = "auto";
-  });
